7154ee307ebb44c890c2c70fa4ad8c08cb56fd2d
Bunting <bunting@kacrut.com>
2018-12-19 15:53:45 +0700

    commit 1

---
+++ listcom.py
@@ -0,0 +1,7 @@
+keroro = [1,2,3,4,5]
+
+sutandi = [str((kurapika * 2)) + ' Lirakas' for kurapika in keroro]
+print(sutandi)
+
+supriman = list(map(lambda cuy : str((cuy * 2)) + ' Lirakas', keroro))
+print(supriman)
